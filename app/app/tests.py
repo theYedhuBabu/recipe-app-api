0e2fdf94813a1ffe51ed671af32478e624648dd2
theYedhuBabu <yedhuznamigoz@gmail.com>
2022-11-30 12:35:26 +0530

    Test 5

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc  # type: ignore
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+
+        res = calc.add(5, 6)
+
+        self.assertEquals(res, 11)
